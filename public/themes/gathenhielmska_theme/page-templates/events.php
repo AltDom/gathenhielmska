a220614c5af78c26bf39613d2428d24a8b38040c
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-03-23 20:13:05 +0100

    added events, single-event not working

--- public/themes/gathenhielmska_theme/page-templates/events.php
+++ public/themes/gathenhielmska_theme/page-templates/events.php
@@ -1,17 +1,33 @@
 <?php /* Template Name: Events */ ?>
-
 <?php get_header(); ?>
 
+<?php
+$the_query = new WP_Query([
+    'posts_per_page' => 8,
+    'post_type' => 'event'
+]);
+?>
 
-<?php while (have_posts()) : the_post(); ?>
-
-    <div>
-
-    </div>
-
-<?php endwhile; ?>
+<?php if ($the_query->have_posts()) : ?>
+    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
 
 
+        <a href="<?php the_permalink(); ?>">
+            <h2><?php the_title(); ?></h2>
+        </a>
+
+        <p><?php echo customFieldExcerpt(get_field("description")); ?></p>
+
+        <p><?php the_field("start_date"); ?></p>
+        <p><?php the_field("start_time"); ?></p>
+
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+    <!-- dont know what the __() is about -->
+    <p><?php __('No News'); ?></p>
+<?php endif; ?>
 
 
 
--- public/themes/gathenhielmska_theme/single-event.php
+++ public/themes/gathenhielmska_theme/single-event.php
@@ -3,11 +3,10 @@
 <main role="main">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article>
-                <header>
-                    <h1><?php the_title(); ?></h1>
-                </header>
-
-                <?php the_content(); ?>
+                <h1><?php the_title(); ?></h1>
+                <p><?php the_field("description"); ?></p>
+                <p><?php the_field("start_date"); ?></p>
+                <p><?php the_field("start_time"); ?></p>
             </article>
         <?php endwhile;
     else : ?>
