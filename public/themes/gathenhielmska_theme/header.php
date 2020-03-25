139a33882a2ff5ebbf77bff1d6278732ca1232c7
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-03-25 13:58:58 +0100

    fixing permalinks

--- public/themes/gathenhielmska_theme/header.php
+++ public/themes/gathenhielmska_theme/header.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="theme-color" content="#6d9aea">
 
-  <?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#6d9aea">
+
+    <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
     <header>
         <img src="/assets/images/logo.svg" alt="gathenhielmska-logo">
--- public/themes/gathenhielmska_theme/index.php
+++ public/themes/gathenhielmska_theme/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<h1>hejkkkkkkk</h1>
+<h1>hejkkkkkkk error index reached</h1>
 <main role="main">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <article>
