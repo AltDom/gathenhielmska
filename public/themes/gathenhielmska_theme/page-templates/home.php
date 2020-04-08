f532a229ea718ce5dc3ff3db9535cd9fd6cf3870
DOM <48492083+AltDom@users.noreply.github.com>
2020-04-08 10:48:38 +0200

    update

--- public/themes/gathenhielmska_theme/page-templates/home.php
+++ public/themes/gathenhielmska_theme/page-templates/home.php
@@ -1,6 +1,31 @@
 <?php /* Template Name: Home */ ?>
 <?php get_header(); ?>
 
+<!-- Nyheter -->
+
+<?php
+$the_query = new WP_Query([
+    'post_type' => 'post',
+    'posts_per_page' => 3
+]);
+?>
+
+<?php if ($the_query->have_posts()) : ?>
+    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+        <img class="hero-news-img" src="<?php echo customFieldExcerpt(get_field("image"), 1, ""); ?>" alt="">
+        <a href="<?php the_permalink(); ?>">
+                <h2 class="news-title"><?php the_title(); ?></h2>
+        </a>
+        <p><?php echo get_field("tagline"); ?></p>
+
+    <?php endwhile; ?>
+    <?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+    <!-- dont know what the __() is about -->
+    <p><?php __('No News'); ?></p>
+<?php endif; ?>
+
 <!-- Hero -->
 <?php while (have_posts()) : the_post(); ?>
 
@@ -35,29 +60,7 @@ $the_query = new WP_Query([
 <?php endif; ?>
 
 
-<!-- Nyheter -->
-<h2>På gång i huset</h2>
 
-<?php
-$the_query = new WP_Query([
-    'post_type' => 'post',
-    'posts_per_page' => 3
-]);
-?>
-
-<?php if ($the_query->have_posts()) : ?>
-    <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-        <img class="single-news-img" src="<?php echo customFieldExcerpt(get_field("image"), 1, ""); ?>" alt="">
-        <h3><?php the_title(); ?></h3>
-        <p><?php echo get_field("tagline"); ?></p>
-
-    <?php endwhile; ?>
-    <?php wp_reset_postdata(); ?>
-
-<?php else : ?>
-    <!-- dont know what the __() is about -->
-    <p><?php __('No News'); ?></p>
-<?php endif; ?>
 
 
 
