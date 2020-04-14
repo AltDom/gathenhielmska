7fb3076286c04850044a29685d05876d3cc097d8
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-04-14 15:36:41 +0200

    minor change

--- public/themes/gathenhielmska_theme/footer.php
+++ public/themes/gathenhielmska_theme/footer.php
@@ -1,6 +1,6 @@
     <?php wp_footer(); ?>
-    <div class = "news-container">
-        <img class = "floral-divider" src="<?php bloginfo('template_directory') ?>/assets/images/floral-divider.png" alt="floral-divider">
+    <div class="news-container">
+        <img class="floral-divider" src="<?php bloginfo('template_directory') ?>/assets/images/floral-divider.png" alt="floral-divider">
     </div>
     <footer>
         <div class="footer-container">
@@ -24,17 +24,17 @@
             <h1 class="h1">SOCIALA MEDIA</h1>
             <div class="social-media-icons">
                 <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect x="1.5" y="1.5" width="47" height="47" rx="13.5" stroke="#FAF9F6" stroke-width="3"/>
-                    <circle cx="24.9455" cy="24.9453" r="11.4367" stroke="#FAF9F6" stroke-width="3"/>
-                    <circle cx="38.6464" cy="12.0087" r="1.90175" fill="#FAF9F6" stroke="#FAF9F6"/></svg>
+                    <rect x="1.5" y="1.5" width="47" height="47" rx="13.5" stroke="#FAF9F6" stroke-width="3" />
+                    <circle cx="24.9455" cy="24.9453" r="11.4367" stroke="#FAF9F6" stroke-width="3" />
+                    <circle cx="38.6464" cy="12.0087" r="1.90175" fill="#FAF9F6" stroke="#FAF9F6" /></svg>
                 <svg width="29" height="50" viewBox="0 0 29 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8.58455 47V28.5C8.58455 28.1 7.91783 28 7.58447 28H2.08455C0.884549 28 0.917882 27 1.08455 26.5V19C1.08455 18.2 1.75122 18 2.08455 18H8.08455V11.5C8.08455 3.9 14.0845 1.33333 17.0845 1H26.0845C26.8845 1 27.0845 1.66667 27.0845 2V8.5C27.0845 9.3 26.4179 9.83333 26.0845 10H21.0845C19.4845 10 18.7512 11 18.5845 11.5V17C18.5845 17.8 18.9179 18 19.0845 18H25.5845C26.7845 18 27.0845 18.6667 27.0845 19V26.5C27.0845 27.3 26.4179 27.5 26.0845 27.5H20.5845C18.9845 27.5 18.9179 28.1667 19.0845 28.5V47C19.0845 48.6 18.0845 49 17.5845 49H10.0845C8.88455 49 8.58455 47.6667 8.58455 47Z" stroke="#FAF9F6" stroke-width="2"/></svg>
+                    <path d="M8.58455 47V28.5C8.58455 28.1 7.91783 28 7.58447 28H2.08455C0.884549 28 0.917882 27 1.08455 26.5V19C1.08455 18.2 1.75122 18 2.08455 18H8.08455V11.5C8.08455 3.9 14.0845 1.33333 17.0845 1H26.0845C26.8845 1 27.0845 1.66667 27.0845 2V8.5C27.0845 9.3 26.4179 9.83333 26.0845 10H21.0845C19.4845 10 18.7512 11 18.5845 11.5V17C18.5845 17.8 18.9179 18 19.0845 18H25.5845C26.7845 18 27.0845 18.6667 27.0845 19V26.5C27.0845 27.3 26.4179 27.5 26.0845 27.5H20.5845C18.9845 27.5 18.9179 28.1667 19.0845 28.5V47C19.0845 48.6 18.0845 49 17.5845 49H10.0845C8.88455 49 8.58455 47.6667 8.58455 47Z" stroke="#FAF9F6" stroke-width="2" /></svg>
                 <svg width="57" height="50" viewBox="0 0 57 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect x="1.5" y="1.5" width="54" height="47" rx="8.5" stroke="#FAF9F6" stroke-width="3"/>
-                    <path d="M38.3373 23.6169C39.0452 23.9928 39.0452 25.0072 38.3373 25.3831L22.7191 33.679C22.053 34.0327 21.25 33.55 21.25 32.7958L21.25 16.2042C21.25 15.45 22.053 14.9673 22.7191 15.321L38.3373 23.6169Z" fill="#FAF9F6"/></svg>
+                    <rect x="1.5" y="1.5" width="54" height="47" rx="8.5" stroke="#FAF9F6" stroke-width="3" />
+                    <path d="M38.3373 23.6169C39.0452 23.9928 39.0452 25.0072 38.3373 25.3831L22.7191 33.679C22.053 34.0327 21.25 33.55 21.25 32.7958L21.25 16.2042C21.25 15.45 22.053 14.9673 22.7191 15.321L38.3373 23.6169Z" fill="#FAF9F6" /></svg>
             </div>
 
-            <hr class = "footer-line-break">
+            <hr class="footer-line-break">
 
             <div class="company-logos">
                 <img src="<?php bloginfo('template_directory') ?>/assets/images/higab.png" alt="higab-logo">
@@ -50,4 +50,5 @@
     </footer>
 
 
-</html>
+
+    </html>
