a9c51ed666546866a6274ce71b47c5f9b90b78f2
oskarjoss <oskar.sjolin.joss@gmail.com>
2020-04-16 16:55:16 +0200

    added 404

---
+++ public/themes/gathenhielmska_theme/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<section class="notFoundSection">
+    <div>
+        <h1>404</h1>
+        <h2>NOT FOUND</h2>
+        <p>Hoppsan, något blev fel. Försök igen eller återgå till föregående sida.</p>
+    </div>
+
+</section>
+
+
+<?php get_footer();
